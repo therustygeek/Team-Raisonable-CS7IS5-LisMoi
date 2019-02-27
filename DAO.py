d031c171cf02a51bb53d2ad849d0676fca4b52cc
XINDI QIU <xqiu@tcd.ie>
2019-02-27 16:23:59 +0000

    add selection function
    add selection function

--- DAO.py
+++ DAO.py
@@ -1,14 +1,20 @@
-#
-# Databse Access Object
-#
-import pymongo, json
-
-
-jsonfile = open('MOCK_DATA.json','r')
-DML = json.load(jsonfile)
-import pymongo
 from pymongo import MongoClient
-client = MongoClient('localhost',27017)
-db = client["book_repository"]
-collection = db.book
-collection.insert(DML)
+import json
+import datetime
+
+Client = MongoClient()
+db = Client['Book']
+collection = db['book_repository']
+
+def create_collection():
+    jsonfile = open('MOCK_DATA.json','r')
+    DML = json.load(jsonfile)
+    collection.insert_many(DML)
+
+def select_data():
+    result=collection.find({'genre':'Comedy'}).limit(10)
+    for data in result:
+        print(data)
+
+select_data()
+
